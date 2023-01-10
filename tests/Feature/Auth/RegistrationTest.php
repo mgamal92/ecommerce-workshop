8fedf858a2153b466fcfa6dd34a4fd29bbc5b0e4
Mohamed Gamal <mg.dev1992@gmail.com>
2023-01-10 16:18:58 +0200

    revert test case

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -11,6 +11,15 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register()
     {
-        $this->assertTrue(true);
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            ''
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertNoContent();
     }
 }
