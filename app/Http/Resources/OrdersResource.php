019f3d766403a4cb2b9f145b2b1e92e8ee8348b7
AhmedMub <mada.mub@gmail.com>
2023-02-05 00:21:21 +0300

    edit api order resource

--- app/Http/Resources/OrdersResource.php
+++ app/Http/Resources/OrdersResource.php
@@ -14,6 +14,12 @@ class OrdersResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => (string) $this->id,
+            'attributes' => [
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ]
+        ];
     }
 }
