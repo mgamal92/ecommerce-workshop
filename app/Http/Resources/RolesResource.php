82d1d1404be405d034f6834dcc5e94b6eb4cbd99
AhmedMub <mada.mub@gmail.com>
2023-02-15 00:11:46 +0300

    roles crud,assign roles, remove roles, add new endpoints

---
+++ app/Http/Resources/RolesResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RolesResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => (string) $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'guard_name' => $this->guard_name,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ]
+        ];
+    }
+}
