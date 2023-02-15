e1fbd9730f588ff075a15b3e19d22bc1abd59813
MahmoudElsherbeny <m.elsherbeny97@gmail.com>
2023-02-15 13:48:21 +0200

    add database tables

--- database/migrations/2023_01_01_122636_create_products_table.php
+++ database/migrations/2023_01_01_122636_create_products_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->integer('category_id');
+            $table->string('name');
+            $table->string('price');
             $table->timestamps();
         });
     }
--- database/migrations/2023_01_01_123456_create_categories_table.php
+++ database/migrations/2023_01_01_123456_create_categories_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('products_count');
             $table->timestamps();
         });
     }
