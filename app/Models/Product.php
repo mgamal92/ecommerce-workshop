c460a0b9079c2e2e1a19a2342ee5293a8e30cf6e
MahmoudElsherbeny <m.elsherbeny97@gmail.com>
2023-02-16 19:20:39 +0200

    preparing task

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Baro\PipelineQueryCollection\Concerns\Filterable;
+use Baro\PipelineQueryCollection\Contracts\CanFilterContract;
+use Baro\PipelineQueryCollection\RelativeFilter;
 
-class Product extends Model
+
+class Product extends Model implements CanFilterContract
 {
-    use HasFactory;
+    use HasFactory, Filterable;
+
+    public function getFilters(): array
+    {
+        return [
+            new RelativeFilter('name'),
+            new RelativeFilter('price'),
+            new RelativeFilter('category'),
+        ];
+    }
 }
