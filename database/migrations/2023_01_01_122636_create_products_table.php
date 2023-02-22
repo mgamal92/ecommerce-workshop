9e1423a384c64925dd0dac99c3e3907bc2a247d6
MahmoudElsherbeny <m.elsherbeny97@gmail.com>
2023-02-22 18:50:35 +0200

    fix tests

--- database/migrations/2023_01_01_122636_create_products_table.php
+++ database/migrations/2023_01_01_122636_create_products_table.php
@@ -15,10 +15,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->integer('category_id');
-            $table->string('name');
-            $table->integer('price');
-            $table->integer('quantity');
             $table->timestamps();
         });
     }
