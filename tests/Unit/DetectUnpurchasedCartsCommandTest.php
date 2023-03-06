c5c374211c1140354a78fb25fcc875eded57cccb
mahmoud <mahmoudghareeb11111@gmail.com>
2023-03-06 15:22:53 +0200

    edit command test

---
+++ tests/Unit/DetectUnpurchasedCartsCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Console\Commands\DetectUnpurchasedCartsCommand;
+use App\Models\Cart;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Console\Application;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+
+use PHPUnit\Framework\TestCase;
+
+class DetectUnpurchasedCartsCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test if the testDetectUnpurchasedCartsCommand works properly.
+     *
+     * @return void
+     */
+    public function testDetectUnpurchasedCartsCommand()
+    {
+        $command = new DetectUnpurchasedCartsCommand();
+        $application = new Application($this->app, $command->getName());
+
+        $cart = Cart::factory()->create([
+            'created_at' => Carbon::yesterday(),
+        ]);
+        Artisan::call('detect:carts');
+        $this->assertTrue(true);
+
+        Artisan::call('queue:work');
+        Queue::assertPushed(function ($job) {
+            return $job === 'Job executed successfully';
+        });
+    }
+}
