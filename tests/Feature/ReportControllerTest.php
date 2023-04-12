3f68c9ecc1043e45b623077f126a60d9d316d61a
AhmedMub <mada.mub@gmail.com>
2023-04-12 12:19:45 +0300

    Report for the members and customers who registered within period

---
+++ tests/Feature/ReportControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ReportControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private User $user;
+    private Customer $customer;
+
+    //setup to run before each test method
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->customer = Customer::factory()->create();
+    }
+
+    /* 
+        /- Test report for the customers who registered within a specific period
+    */
+    public function test_report_for_customers_who_registered_within_specific_period()
+    {
+        $date = $this->customer->first()->created_at->format('d-m-Y');
+
+        $response = $this->actingAs($this->user)->getJson(route('report.customers', ['from' => $date, 'to' => $date]));
+
+        $response->assertSuccessful();
+    }
+
+    /* 
+        /- Test report for the members who registered within a specific period
+    */
+    public function test_report_for_members_who_registered_within_specific_period()
+    {
+        $date = $this->user->first()->created_at->format('d-m-Y');
+
+        $response = $this->actingAs($this->user)->getJson(route('report.members', ['from' => $date, 'to' => $date]));
+
+        $response->assertSuccessful();
+    }
+}
